83b0807d57fcbd6a4ec41afe8a44959fb2c4b2b1
심다슬 <49306239+daseul22@users.noreply.github.com>
2021-04-14 22:32:37 +0900

    01: String

--- simdaseul/01-p647.js
+++ simdaseul/01-p647.js
@@ -1,1 +1,30 @@
-test
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) { 
+    
+    let sum = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        for(let j = 1 + i; j <= s.length; j++){
+            
+            let subs = s.slice(i,j);
+            
+            let isPal = true;
+            for(let k = 0; k < Math.round(subs.length/2); k++){
+                
+                if(subs[k] !== subs[subs.length - k - 1]){
+                    isPal = false;
+                    break;
+                }
+            }
+            if(isPal){
+                sum++;
+            }
+           
+        }
+    }
+    return sum;
+    
+};
