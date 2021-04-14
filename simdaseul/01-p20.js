7aa9e50fca3f96d6ea152410932a58bd8e4f5b62
심다슬 <49306239+daseul22@users.noreply.github.com>
2021-04-14 21:51:02 +0900

    01 : String

--- simdaseul/01-p20.js
+++ simdaseul/01-p20.js
@@ -1,1 +1,35 @@
-test
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    if(s.length % 2) return false; 
+    
+    let stack = [];
+    let count = 0;
+    
+    for(let bracket of s){
+        if(bracket === "(" || bracket === "[" || bracket === "{"){
+            stack.push(bracket);
+            ++count; 
+        }else if(bracket === ")" || bracket === "]" || bracket === "}"){
+            let pop = stack.pop();
+            --count;
+            if(pop === "(" && bracket === ")"){
+                continue;
+            }else if(pop === "[" && bracket === "]"){
+                continue;
+            }else if(pop === "{" && bracket === "}"){
+                continue;
+            }else{
+                return false
+            }
+            
+        }
+    }
+    
+    if(count) return false;
+    
+    return true;
+};
