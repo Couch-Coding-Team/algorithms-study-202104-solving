fa9235df60a8d592e8e2bf36121b0b11fce7d045
Sujin Lee <krispediadot@gmail.com>
2021-04-20 11:01:09 +0900

    Solved: leetcode 114.Flatten Binary Tree to Linked List

---
+++ krispediadot/02-leetcode-p114.cpp
@@ -0,0 +1,24 @@
+// 4ms, 12.6MB
+class Solution {
+public:
+    TreeNode* findLeftLeap(TreeNode* leftNode) {
+        while (leftNode->right != nullptr) {
+            leftNode = leftNode->right;
+        }
+        return leftNode;
+    }
+    void flatten(TreeNode* root) {
+        if (root == nullptr || (root->left == nullptr && root->right == nullptr)) {
+            return;
+        }
+        
+        if (root->left != nullptr) flatten(root->left);
+        if (root->right != nullptr) flatten(root->right);
+
+        if (root->left != nullptr) {
+            findLeftLeap(root->left)->right = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+        }
+    }
+};
