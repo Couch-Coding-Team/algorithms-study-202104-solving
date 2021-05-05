bd422629e2860fa0e8b176c81242956f46e95aa0
Park Sang Yun <71218974+pa324@users.noreply.github.com>
2021-05-05 13:22:41 +0900

    Update Week4PartitionLabels.java

--- pa324/week1/Week4PartitionLabels.java
+++ pa324/week1/Week4PartitionLabels.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<Integer> partitionLabels(String S) {
-        if(S==null || S.length() == 0) {
-            return null;
-        }
-        if(S.charAt(0) == S.charAt(S.length() - 1)) {
-            return List.of(S.length());
-        }
         List<Integer> result = new ArrayList<>();
-        Map<Character,Integer> map = new HashMap<>();
+        if(S.charAt(0) == S.charAt(S.length()-1)) {
+            result.add(S.length());
+            return result;
+        }
+        
+        HashMap<Character,Integer> map =  new HashMap<>();
         for(int i = 0; i < S.length(); i++) {
             map.put(S.charAt(i),i);
         }
-        int firstIndex = 0;
-        int lastIndex = 0;
+        
+        int max = 0;
+        int start = 0;
         for(int i = 0; i < S.length(); i++) {
-            lastIndex = Math.max(lastIndex, map.get(S.charAt(i)));
-            if(i == lastIndex) {
-                result.add(lastIndex - firstIndex + 1);
-                firstIndex = lastIndex + 1;
+            max = Math.max(max, map.get(S.charAt(i)));
+            if(i == max) {
+                result.add(max - start + 1);
+                start = i+1;
+                max = 0;
             }
         }
         
         return result;
+        
     }
 }
