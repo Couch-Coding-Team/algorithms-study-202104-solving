b843d6835c46db5a237a42c8b39d7d38dabd37c2
Sujin Lee <krispediadot@gmail.com>
2021-05-18 16:30:37 +0900

    Solved: leetcode 494.Target Sum dp 방식으로 풀이

--- krispediadot/05-leetcode-p494.cpp
+++ krispediadot/05-leetcode-p494.cpp
@@ -21,3 +21,27 @@ public:
         return answer;
     }
 };
+
+class Solution2 {
+    vector<vector<int>> dp;
+public:
+    int calcNums(vector<int>& nums, int idx, int sum, int target) {
+        if (idx == nums.size()) return sum == target ? 1 : 0;
+        if (dp[idx][sum + 1000] != -1) return dp[idx][sum + 1000];
+
+        int cnt = 0;
+
+        // +
+        cnt += calcNums(nums, idx + 1, sum + nums[idx], target);
+
+        // -
+        cnt += calcNums(nums, idx + 1, sum - nums[idx], target);
+
+        return dp[idx][sum + 1000] = cnt;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        dp.resize(21, vector<int>(2001, -1));
+        return calcNums(nums, 0, 0, target);
+    }
+};
