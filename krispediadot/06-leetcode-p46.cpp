4f316db421958d78ca2d6e1fd39d4948ee31acf0
Sujin Lee <krispediadot@gmail.com>
2021-05-18 17:42:51 +0900

    Solved: leetcode 46. Permutations

---
+++ krispediadot/06-leetcode-p46.cpp
@@ -0,0 +1,15 @@
+// 0ms, 7.5MB, 00:02:20
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> answer;
+        
+        sort(nums.begin(), nums.end());
+        
+        do {
+            answer.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+    
+        return answer;
+    }
+};
