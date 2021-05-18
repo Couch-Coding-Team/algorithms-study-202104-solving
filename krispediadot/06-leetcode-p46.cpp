1c7c6a2c040c58805030dad30584c25cbdec112a
Sujin Lee <krispediadot@gmail.com>
2021-05-18 17:56:56 +0900

    Solved: leetcode 46. Permutations

--- krispediadot/06-leetcode-p46.cpp
+++ krispediadot/06-leetcode-p46.cpp
@@ -13,3 +13,31 @@ public:
         return answer;
     }
 };
+
+// 0ms, 9.1MB
+class Solution2 {
+    vector<vector<int>> answer;
+public:
+    void generator(vector<int> unique_set, vector<int> temp) {
+        if (unique_set.size() == 0) {
+            answer.push_back(temp);
+            return;
+        }
+
+        for (auto target : unique_set) {
+            vector<int> unique_set_copy = unique_set;
+            unique_set_copy.erase(find(unique_set_copy.begin(), unique_set_copy.end(), target));
+            temp.push_back(target);
+            generator(unique_set_copy, temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> unique_set(nums.begin(), nums.end());
+
+        vector<int> temp;
+        generator(unique_set, temp);
+
+        return answer;
+    }
+};
