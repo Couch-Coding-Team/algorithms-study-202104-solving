1fcf6d89acd0bd35ce15bfb0903f0cd6de421158
Park Sang Yun <71218974+pa324@users.noreply.github.com>
2021-06-01 23:12:27 +0900

    Create CoinChange.java

---
+++ pa324/week1/CoinChange.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //dp[i] = i원을 만드는데 필요한 동전의 최소 개수
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+
